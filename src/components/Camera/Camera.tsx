9ca5467997236feed85fe52b85f0a59cb3249081
Pavol Hejny <me@pavolhejny.com>
2018-05-26 01:38:15 +0200

    webcam

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -2,10 +2,28 @@ import * as React from 'react';
 import * as Webcam from 'react-webcam';
 import './Camera.css';
 
-export function Camera({}:{}){
-  return (
-    <div className="Camera">
-      <Webcam/>
-    </div>
-  );
+export class Camera extends React.Component {
+
+  private webcam:any;
+
+  capture(){
+    const imageSrc = this.webcam.getScreenshot();
+    console.log(imageSrc);
+  };
+
+  render() {
+    return (
+      <div className={'Camera'}  onClick={()=>this.capture()}>
+        <Webcam
+          audio={false}
+          width={window.innerWidth}
+          height={window.innerHeight}
+          ref={(webcam)=>this.webcam=webcam}
+          screenshotFormat="image/jpeg"
+          
+        />
+        <button></button>
+      </div>
+    );
+  }
 }
