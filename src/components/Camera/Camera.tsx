f03cb575462639b55d016ea401e17f33dac77e79
Pavol Hejny <me@pavolhejny.com>
2018-05-26 01:04:16 +0200

    webcam

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import * as Webcam from 'react-webcam';
 import './Camera.css';
 
 export function Camera({}:{}){
   return (
     <div className="Camera">
-
+      <Webcam/>
     </div>
   );
 }
