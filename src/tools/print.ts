d9e15d7bf8c31a096cb042e8cb4759e55732b859
Pavol Hejny <me@pavolhejny.com>
2018-05-27 12:23:39 +0200

    printing

--- src/tools/print.ts
+++ src/tools/print.ts
@@ -1,15 +1,62 @@
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
+import * as superagent from 'superagent';
 
-export function print() {
+
+function blobToDataURL(blob:Blob):Promise<string> {
+    return new Promise((resolve,reject)=>{
+        var a = new FileReader();
+        a.onload = function(e) {
+            resolve((e.target as any).result);
+        }
+        a.readAsDataURL(blob);
+    }); 
+}
+
+export async function print() {
     //todo
     jsPDF;
     html2canvas;
 
-    const printElement = document.getElementById('print'); //todo better
-    const printCanvas = html2canvas(printElement!);
+    const printElement = document.querySelector('#print table') as HTMLElement; //todo better
+    console.log(printElement);
+
+     //---------
+
+    var images = printElement.getElementsByTagName('img');
+    for (let i = 0, l = images.length; i < l; i++){
+        
+        const image = images[i];
+        const result = await superagent.get(image.src).responseType('blob');
+        const dataUrl = await blobToDataURL(result.body);
+        image.src = dataUrl;
+    }
+
+    //---------
+    /**/
+
+    const printCanvas = await html2canvas(printElement!,{allowTaint:false});
 
     console.log(printCanvas);
+
+    document.body.appendChild(printCanvas);
+
+
+    //---------
+    /**/
+
+    const width = 58;
+    const height = printCanvas.height*width/printCanvas.width;
+
+    // only jpeg is supported by jsPDF
+    var imgData = printCanvas.toDataURL("image/jpeg", 1.0);
+    var pdf = new jsPDF('p', 'mm', [width, height] as any);
+
+    pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
+    pdf.save("download.pdf");
+
+    
+    //console.log(printCanvas.toDataURL());
 
     /*var doc = new jsPDF();
     doc.setFontSize(40)
